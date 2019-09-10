cc798febffaa538d287d62ebc76b6670254b1d23
dockdep <dockdep@gmail.com>
2019-09-11 00:19:32 +0300

    price

---
+++ Book/js/lesson.js
@@ -0,0 +1,11 @@
+switch(a){
+    case 6:
+        console.log('six');
+        break;
+    case 8:
+        console.log('eight');
+        break;
+    default:
+        console.log('test');
+        break;
+}
